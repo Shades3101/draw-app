43b7bf81e3134ec5ac512006a3ca307b42089952
Shades3101 <karannarania12@gmail.com>
2026-02-08 12:09:31 +0530

    compile shared packages to cjs dist and http backend cors

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -9,7 +9,10 @@ import cors from 'cors';
 
 const app = express();
 app.use(express.json());
-app.use(cors())
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true
+}))
 
 app.post("/signup", async (req, res) => {
 
